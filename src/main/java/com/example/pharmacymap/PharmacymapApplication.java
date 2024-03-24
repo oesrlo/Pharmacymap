fbd5e373ad8a7240a7d3a58ce64362898ba9fc67
oesrlo <yiyah9901@gmail.com>
2024-03-24 15:56:53 +0900

    3.24 setting

---
+++ src/main/java/com/example/pharmacymap/PharmacymapApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacymap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacymapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacymapApplication.class, args);
+    }
+
+}
